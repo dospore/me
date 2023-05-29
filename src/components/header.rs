7d5e3e0729a62e8793632004b496b36b54124b96
Dospore <tetthera@gmail.com>
2023-05-29 13:52:17 +0200

    Add fonts

--- src/components/header.rs
+++ src/components/header.rs
@@ -3,13 +3,17 @@ use yew::prelude::*;
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
-        <div>
+        <div class="nav">
                         <span class="link">
-                            <a href="/projects">{"/projects"}</a>
+                            <a href="/">{"cd"}</a>
                         </span>
                         <span class="seperator">{"|"}</span>
                         <span class="link">
-                            <a href="/collection">{"/collection"}</a>
+                            <a href="/projects">{"cd /projects"}</a>
+                        </span>
+                        <span class="seperator">{"|"}</span>
+                        <span class="link">
+                            <a href="/collection">{"cd /collection"}</a>
                         </span>
         </div>
     }
--- src/pages/collection.rs
+++ src/pages/collection.rs
@@ -8,9 +8,22 @@ use crate::components::animated_image::AnimatedImage;
 pub fn collection() -> Html {
     html! {
         <div class="collection">
-            <AnimatedImage collection="broker" />
-            <AnimatedImage collection="punk" />
-            <AnimatedImage collection="captain" />
+            <h1>{"The Vault"}</h1>
+            <p>
+                {"If I had to make a justification on my gambling addictions I'd say I like jpegs which in some way are technologically innovative. "}
+                {"Don't be fooled into thinking this is a successful collection. I bought many"}
+                <span class="redacted">
+                    <span>{" ["}</span>
+                    <span>{"redacted"}</span>
+                    <span>{"] "}</span>
+                </span>
+                {"jpegs which are ultimately worthless."}
+            </p>
+            <div class="assets">
+                <AnimatedImage collection="broker" />
+                <AnimatedImage collection="punk" />
+                <AnimatedImage collection="captain" />
+            </div>
         </div>
     }
 }
